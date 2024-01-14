522dfe909f4912f66c4cd416128426b8937c5351
Aaron Cooper <snazzie@snazzie.space>
2024-01-14 08:15:41 +0000

    .

--- Source/Events/Events.Server/Data/ApplicationUser.cs
+++ Source/Events/Events.Server/Data/ApplicationUser.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 
 public class ApplicationUser : IdentityUser
 {
