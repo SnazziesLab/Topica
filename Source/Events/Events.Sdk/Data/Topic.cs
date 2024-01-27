9f35788ce779b567c980f7d96bb0fcffab78f570
Aaron Cooper <snazzie@snazzie.space>
2024-01-27 00:26:16 +0000

    .

--- Source/Events/Events.Sdk/Data/Topic.cs
+++ Source/Events/Events.Sdk/Data/Topic.cs
@@ -5,7 +5,6 @@ public class Topic
 {
     public required string Name { get; set; }
     public DateTimeOffset CreatedOn { get; set; } = DateTimeOffset.Now;
-    public string Description { get; set; } = string.Empty;
     public  List<Entry> History { get; set; } = new();
 
 }
