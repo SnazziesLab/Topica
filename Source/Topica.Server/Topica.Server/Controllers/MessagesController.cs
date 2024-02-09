86ed0e71edd7b57f9e51f7ddc9dbcd6fda1ff3d3
Aaron Cooper <snazzie@snazzie.space>
2024-02-09 13:10:43 +0000

    Update MessagesController.cs

--- Source/Topica.Server/Topica.Server/Controllers/MessagesController.cs
+++ Source/Topica.Server/Topica.Server/Controllers/MessagesController.cs
@@ -36,5 +36,25 @@ namespace Topica.Server.Controllers
 
             return Ok(topicId);
         }
+        /// <summary>
+        /// Creates a message under topic id.
+        /// </summary>
+        /// <param name="topicId">If topicId is null, a new Topic will be generated with a random GUID</param>
+        /// <param name="message"></param>
+        /// <returns>Topic Id</returns>
+        [ProducesResponseType<string>(StatusCodes.Status200OK)]
+        [Authorize(Roles = "write")]
+        [HttpPost(Name = nameof(AddMessage))]
+        public async Task<ActionResult<string>> AddMessage(string? topicId, [FromBody] string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                return BadRequest($"{nameof(message)} cannot be null");
+            if (string.IsNullOrEmpty(topicId))
+                topicId = Guid.NewGuid().ToString();
+            var msg = new Message() { TopicId = topicId, Content = message };
+            await Store.AddMessageAsync(msg);
+
+            return Ok(topicId);
+        }
     }
 }
